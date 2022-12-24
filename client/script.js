c0b3c1a53b8b725e62450370eb114086fc9a3c31
priyanshu shrivastava <99priyanshu33@gmail.com>
2022-12-24 18:09:25 +0530

    g

--- client/script.js
+++ client/script.js
@@ -206,7 +206,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('https://askfrom.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
